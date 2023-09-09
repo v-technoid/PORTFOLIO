b9551744d9f3718757ddf6d8132d9195adffcd41
Vishnuppriyan PL <vishnutechnophile@gmail.com>
2023-09-09 19:44:06 +0530

    Update 3.0

--- main.js
+++ main.js
@@ -6,35 +6,10 @@ const typed = new Typed('.text', {
     loop: true
   });
 
+  const fadeOut= () => {
+    const loaderWrapper = document.querySelector('.lds-heart');
+    loaderWrapper.classList.add('.fade');
+  }
+  window.addEventListener('load', fadeOut);
   
 // Target the elements you want to animate
-const logo = document.querySelector('.logo');
-const navbarLinks = document.querySelectorAll('.navbar a');
-const homeContent = document.querySelector('.home-content');
-
-// Define the animations
-const logoAnimation = anime({
-  targets: logo,
-  opacity: [0, 1],
-  translateX: [-100, 0],
-  duration: 1000,
-  easing: 'easeOutExpo',
-});
-
-const navbarAnimation = anime({
-  targets: navbarLinks,
-  opacity: [0, 1],
-  translateY: [100, 0],
-  delay: anime.stagger(200, { start: 500 }),
-  duration: 1000,
-  easing: 'easeOutExpo',
-});
-
-const homeContentAnimation = anime({
-  targets: homeContent,
-  opacity: [0, 1],
-  translateY: [100, 0],
-  delay: 1500,
-  duration: 1000,
-  easing: 'easeOutExpo',
-});
